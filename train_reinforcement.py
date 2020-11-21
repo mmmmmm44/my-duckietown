1a856d06ce49b95db6b363b60433197dddeb7b2b
mmmmmm44 <pakyimcheng@gmail.com>
2020-11-21 15:00:46 +0800

    Add comments abt hyper-parameters of training ddpg
    More details are in my goodnotes note,
    including which one should we tune and how.

--- train_reinforcement.py
+++ train_reinforcement.py
@@ -127,6 +127,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--start_timesteps", default=1e4, type=int
     )  # How many time steps purely random policy is run for
+    # so after the start_timesteps, the agent runs with a deterministic policy
     parser.add_argument("--eval_freq", default=5e3, type=float)  # How often (time steps) we evaluate
     parser.add_argument("--max_timesteps", default=1e6, type=float)  # Max time steps to run environment for
     parser.add_argument("--save_models", action="store_true", default=True)  # Whether or not models are saved
@@ -139,7 +140,7 @@ if __name__ == "__main__":
     )  # Noise added to target policy during critic update
     parser.add_argument("--noise_clip", default=0.5, type=float)  # Range to clip target policy noise
     parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
-    parser.add_argument("--env_timesteps", default=500, type=int)  # Frequency of delayed policy updates
+    parser.add_argument("--env_timesteps", default=500, type=int)  # Frequency of delayed policy updates <-- this is the real one
     parser.add_argument(
         "--replay_buffer_max_size", default=10000, type=int
     )  # Maximum number of steps to keep in the replay buffer
